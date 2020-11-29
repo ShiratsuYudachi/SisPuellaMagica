b37fcec9c53dd17ec77c1eaab992ca2cc388c895
ShiratsuYudachi <shiratsuyudachi@gmail.com>
2020-11-29 21:41:23 +0800

    make grief seed combustible

--- src/main/java/madokamod/sispuellamagica/crafting/FurnaceRecipeRegistryHandler.java
+++ src/main/java/madokamod/sispuellamagica/crafting/FurnaceRecipeRegistryHandler.java
@@ -2,10 +2,25 @@ package madokamod.sispuellamagica.crafting;
 
 import madokamod.sispuellamagica.item.ItemRegistryHandler;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class FurnaceRecipeRegistryHandler {
     public static void register(){
         GameRegistry.addSmelting(ItemRegistryHandler.GRIEF_PICKAXE,new ItemStack(ItemRegistryHandler.ITEM_GRIEF_SEED),.01F);
     }
+
+    /*
+    @SubscribeEvent
+    public static void onFurnaceFuelBurnTime(FurnaceFuelBurnTimeEvent event){
+        ResourceLocation registryName = event.getItemStack().getItem().getRegistryName();
+        String registryNameResourceDomain = registryName.getResourceDomain();
+        String registryNameResourcePath = registryName.getResourcePath();
+        if ("minecraft".equals(registryNameResourceDomain)&&"stick".equals(registryNameResourcePath)){
+            event.setBurnTime(20);
+        }
+    }
+    */
 }
--- src/main/java/madokamod/sispuellamagica/item/ItemGriefSeed.java
+++ src/main/java/madokamod/sispuellamagica/item/ItemGriefSeed.java
@@ -3,11 +3,19 @@ package madokamod.sispuellamagica.item;
 import madokamod.sispuellamagica.SisPuellaMagica;
 import madokamod.sispuellamagica.creativetab.TabSisPuellaMagica;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class ItemGriefSeed extends Item{
+    @Override
+    public int getItemBurnTime(ItemStack itemStack) {
+        return 12000;
+    }
+
     public ItemGriefSeed() {
         this.setUnlocalizedName(SisPuellaMagica.MODID+".griefSeed");
         this.setRegistryName("grief_seed");
         this.setCreativeTab(TabSisPuellaMagica.TAB_SPM);
+
+
     }
 }
